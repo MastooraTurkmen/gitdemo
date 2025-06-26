47da2bd0b9b809937a857bd1f584d50c2dc1ad82
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-06-26 21:46:49 +0430

    finished website

--- iphone-3d-website-starter/src/components/DisplaySection.jsx
+++ iphone-3d-website-starter/src/components/DisplaySection.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const DisplaySection = () => {
-  return <div></div>;
+const DisplaySection = ({ triggerPreview }) => {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="display-section wrapper">
+      <h2 className="title">New</h2>
+      <p className="text">Brilliant.</p>
+      <span className="description">
+        From $41.62/mo. for 24 mo. or $999 before trade-in.
+      </span>
+      <button className="button" onClick={triggerPreview}>
+        Try Me!
+      </button>
+      <button onClick={handleScrollToTop} className="back-button">
+        TOP
+      </button>
+    </div>
+  );
 };
 
 export default DisplaySection;
